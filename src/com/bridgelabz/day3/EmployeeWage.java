630975458de567d1eb4113677be964437e5ca116
Akanksha-H <aakanksha0499@gmail.com>
2023-01-20 09:09:05 +0530

    Added_the_employee_day_part_time_wage

--- src/com/bridgelabz/day3/EmployeeWage.java
+++ src/com/bridgelabz/day3/EmployeeWage.java
@@ -3,16 +3,20 @@ package com.bridgelabz.day3;
 public class EmployeeWage {
     static int DAILY_WORKING_HOURS = 8;
     static int DAILY_WAGE = 20;
-    static int DAY_CHECK = 1;
+    static int PART_TIME_HOURS = 4;
 
     public static void main(String[] args) {
         System.out.println("WELCOME_EMPLOYEE_TO_DAILY_WAGE_COMPUTATION");
         int empDailyWage = DAILY_WAGE * DAILY_WORKING_HOURS;
+        int empPartTimeWage = DAILY_WAGE * PART_TIME_HOURS;
         // Computation
-        double empDailyCheck = Math.floor(Math.random() * 10) % 2;
-        if (empDailyCheck == DAY_CHECK)
-        System.out.println("Employee is Present Today"+ "\r\n" +"Total Wage for Today = " + empDailyWage);
+        double empDailyCheck = Math.floor(Math.random() * 10) % 3;
+
+        if (empDailyCheck == 1)
+            System.out.println("Employee is Present Today"+ "\r\n" +"Total Wage for Today = " + empDailyWage);
+        else if (empDailyCheck == 2)
+            System.out.println("Employee is Present and Working Part Time today" + "\r\n" + "Total Wage for Today = " + empPartTimeWage);
         else
-        System.out.println("Employee is Absent Today");
+            System.out.println("Employee is Absent Today");
     }
 }
