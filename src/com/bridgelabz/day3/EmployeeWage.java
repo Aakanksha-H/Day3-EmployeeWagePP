6a2ef37741a268cf1a307f3760ffc428b8b9bb8e
Akanksha-H <aakanksha0499@gmail.com>
2023-01-20 09:40:10 +0530

    Added_the_employee_monthly_wage

--- src/com/bridgelabz/day3/EmployeeWage.java
+++ src/com/bridgelabz/day3/EmployeeWage.java
@@ -6,25 +6,46 @@ public class EmployeeWage {
     static final int PART_TIME = 2;
 
     public static void main(String[] args) {
+        System.out.println("WELCOME_EMPLOYEE_TO_DAILY_WAGE_COMPUTATION");
         int workingHours = 0;
+        int totalWorkingDays = 0;
+        int totalWorkingHours = 0;
+
         // Computation
-        int dayCheck = (int) (Math.round(Math.random() * 10) % 3);
+        while (totalWorkingDays < 20) {
+            int dayCheck = (int) (Math.round(Math.random() * 10) % 3);
 
-        switch (dayCheck) {
-            case FULL_TIME:
-                workingHours = 8;
-                int empDailyWage = DAILY_WAGE * workingHours;
-                System.out.println("Employee is Present Today"+ "\r\n" +"Total Wage for Today = " + empDailyWage);
-                break;
+            switch (dayCheck) {
+                case FULL_TIME:
+                    int empDailyWage = DAILY_WAGE * workingHours;
 
-            case PART_TIME:
-                workingHours = 4;
-                int empPartTimeWage = DAILY_WAGE * workingHours;
-                System.out.println("Employee is Present and Working Part Time today" + "\r\n" + "Total Wage for Today = " + empPartTimeWage);
-                break;
+                    workingHours = 8;
 
-            default:
-                System.out.println("Employee is Absent");
+                    totalWorkingHours = workingHours + totalWorkingHours;
+                    totalWorkingDays++;
+
+                    System.out.println("Employee is Present Today" + "\r\n" + "Total Wage for Today = " + empDailyWage);
+                    System.out.println("Employee total working hours = " + totalWorkingHours );
+                    break;
+
+                case PART_TIME:
+                    int empDailyPartTimeWage = DAILY_WAGE * workingHours;
+
+                    workingHours = 4;
+
+                    totalWorkingHours = workingHours + totalWorkingHours;
+                    totalWorkingDays++;
+
+                    System.out.println("Employee is Present and Working Part Time today" + "\r\n" + "Total Wage for Today = " + empDailyPartTimeWage);
+                    System.out.println("Employee total working hours = " + totalWorkingHours);
+                    break;
+
+                default:
+                    System.out.println("Employee is Absent");
+            }
+            System.out.println(totalWorkingDays);
+            int totalWage = totalWorkingHours * DAILY_WAGE;
+            System.out.println("Wage for an month = " + totalWage);
         }
     }
 }
