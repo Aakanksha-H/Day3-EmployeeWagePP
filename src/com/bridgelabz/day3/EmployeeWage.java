39d71756c4b5bdd9208b2eb10c55e7dae1126dd2
Akanksha-H <aakanksha0499@gmail.com>
2023-01-20 09:22:56 +0530

    Refacored_using_the_switch_case

--- src/com/bridgelabz/day3/EmployeeWage.java
+++ src/com/bridgelabz/day3/EmployeeWage.java
@@ -1,22 +1,30 @@
 package com.bridgelabz.day3;
 
 public class EmployeeWage {
-    static int DAILY_WORKING_HOURS = 8;
-    static int DAILY_WAGE = 20;
-    static int PART_TIME_HOURS = 4;
+    static final int DAILY_WAGE = 20;
+    static final int FULL_TIME = 1;
+    static final int PART_TIME = 2;
 
     public static void main(String[] args) {
-        System.out.println("WELCOME_EMPLOYEE_TO_DAILY_WAGE_COMPUTATION");
-        int empDailyWage = DAILY_WAGE * DAILY_WORKING_HOURS;
-        int empPartTimeWage = DAILY_WAGE * PART_TIME_HOURS;
+        int workingHours = 0;
         // Computation
-        double empDailyCheck = Math.floor(Math.random() * 10) % 3;
+        int dayCheck = (int) (Math.round(Math.random() * 10) % 3);
 
-        if (empDailyCheck == 1)
-            System.out.println("Employee is Present Today"+ "\r\n" +"Total Wage for Today = " + empDailyWage);
-        else if (empDailyCheck == 2)
-            System.out.println("Employee is Present and Working Part Time today" + "\r\n" + "Total Wage for Today = " + empPartTimeWage);
-        else
-            System.out.println("Employee is Absent Today");
+        switch (dayCheck) {
+            case FULL_TIME:
+                workingHours = 8;
+                int empDailyWage = DAILY_WAGE * workingHours;
+                System.out.println("Employee is Present Today"+ "\r\n" +"Total Wage for Today = " + empDailyWage);
+                break;
+
+            case PART_TIME:
+                workingHours = 4;
+                int empPartTimeWage = DAILY_WAGE * workingHours;
+                System.out.println("Employee is Present and Working Part Time today" + "\r\n" + "Total Wage for Today = " + empPartTimeWage);
+                break;
+
+            default:
+                System.out.println("Employee is Absent");
+        }
     }
 }
