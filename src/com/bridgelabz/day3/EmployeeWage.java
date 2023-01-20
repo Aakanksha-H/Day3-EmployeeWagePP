80029f4f4970146107048eabdf41733cf84427a0
Akanksha-H <aakanksha0499@gmail.com>
2023-01-20 08:56:08 +0530

    Added_the_employee_day_attendance

--- src/com/bridgelabz/day3/EmployeeWage.java
+++ src/com/bridgelabz/day3/EmployeeWage.java
@@ -1,7 +1,15 @@
 package com.bridgelabz.day3;
 
 public class EmployeeWage {
+    static int DAY_CHECK = 1;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Problem");
+            // Computation
+            double empDailyCheck = Math.floor(Math.random() * 10) % 2;
+            if (empDailyCheck == DAY_CHECK)
+                System.out.println("Employee is Present Today");
+            else
+                System.out.println("Employee is Absent Today");
     }
 }
