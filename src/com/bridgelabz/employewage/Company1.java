7b7bbbc8fba018a0b4863820c2de41f3bdac4b7f
Akanksha-H <aakanksha0499@gmail.com>
2023-02-08 21:54:59 +0530

    Added total wage for company

---
+++ src/com/bridgelabz/employewage/Company1.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employewage;
+
+public class Company1 {
+    public static void main(String[] args) {
+        System.out.println("WELCOME_EMPLOYEE_TO_DAILY_WAGE_COMPUTATION");
+        Main main = new Main();
+        main.calculationOfWage(30,2,10);
+    }
+}
---
+++ src/com/bridgelabz/employewage/Company2.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employewage;
+
+public class Company2 {
+    public static void main(String[] args) {
+        System.out.println("WELCOME_EMPLOYEE_TO_DAILY_WAGE_COMPUTATION");
+        Main main = new Main();
+        main.calculationOfWage(35,25,125);
+    }
+}
